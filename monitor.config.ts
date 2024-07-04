93ed5c9a63f45026c7753438cb9947e70bca69f9
yekta <ngyekta@gmail.com>
2024-07-05 00:10:38 +0300

    Example monitor config

--- monitor.config.ts
+++ monitor.config.ts
@@ -1,23 +1,47 @@
 import type { TMonitor } from './monitor.types';
 
 export const monitors: TMonitor[] = [
+	// Example monitors
+	// Remove the examples and add your own below. ID, title and functionToRun are required.
 	{
 		id: 'website',
 		title: 'Website',
+		// Simple example to check if a website is up
 		functionToRun: async () => {
+			const res = await fetch('https://example.com');
+			if (res.status !== 200) {
+				return false;
+			}
 			return true;
 		}
 	},
 	{
 		id: 'api',
 		title: 'API',
+		// An example with multiple async processes
 		functionToRun: async () => {
-			return true;
+			function waitAndGetRandomNumber() {
+				return new Promise<number>((resolve) => {
+					setTimeout(() => {
+						const randomNumber = Math.random();
+						resolve(randomNumber);
+					}, 1000);
+				});
+			}
+
+			const randomNumberPromise1 = waitAndGetRandomNumber();
+			const randomNumberPromise2 = waitAndGetRandomNumber();
+
+			const result = await Promise.all([randomNumberPromise1, randomNumberPromise2]);
+			const isAllGood = result.every((number) => number > 0.2);
+
+			return isAllGood;
 		}
 	},
 	{
 		id: 'docs',
 		title: 'Docs',
+		// An example with the bare minimum
 		functionToRun: async () => {
 			return true;
 		}
