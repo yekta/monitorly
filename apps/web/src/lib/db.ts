0b4f2f523aadd0e8918d7605e903dce9c512f29a
yekta <ngyekta@gmail.com>
2024-07-05 18:32:01 +0300

    Log url

--- apps/web/src/lib/db.ts
+++ apps/web/src/lib/db.ts
@@ -1,6 +1,7 @@
 import { createDb } from '@statusly/worker/db-web';
 
 const databaseUrl = process.env.DATABASE_URL!;
+console.log('--------- DATABASE URL IS', databaseUrl);
 if (!databaseUrl) {
   throw new Error('DATABASE_URL is not set');
 }
