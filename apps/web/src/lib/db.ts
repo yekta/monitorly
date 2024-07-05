be30978497c044cfdbdab14ecee2752e3e7af2b5
yekta <ngyekta@gmail.com>
2024-07-05 23:07:35 +0300

    Error text tweak

--- apps/web/src/lib/db.ts
+++ apps/web/src/lib/db.ts
@@ -2,7 +2,7 @@ import { createDb } from '@statusly/worker/db-utils';
 
 const databaseUrl = process.env.DATABASE_URL!;
 if (!databaseUrl) {
-  throw new Error('NextJS: DATABASE_URL is not defined');
+  throw new Error('DATABASE_URL is not defined');
 }
 
 export const db = createDb(databaseUrl);
