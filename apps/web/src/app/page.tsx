23bf0323dd6237316b6cbe464d4faf9db7b600e1
yekta <ngyekta@gmail.com>
2024-07-05 22:05:13 +0300

    Dynamic rendering

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,16 +2,14 @@ import { LatestCheck } from '@/components/latest-check';
 import MonitorCard from '@/components/monitor-card/monitor-card';
 import { getMonitors } from '@/lib/queries';
 import { CheckCircleIcon } from '@heroicons/react/24/solid';
-
-export const revalidate = 60;
+import { headers } from 'next/headers';
 
 export default async function Home() {
-  const renderTime = new Date().toISOString();
+  headers();
   const { data } = await getMonitors();
   const latestDate = new Date(Math.max(...data.map((m) => m.latestTimestamp)));
   return (
     <main className="flex w-full flex-1 flex-col items-center justify-start px-5 pb-24 pt-12">
-      <p className="absolute left-4 top-3 text-xs text-foreground-muted">{renderTime}</p>
       <div className="my-auto flex w-full flex-col items-center">
         <div className="flex w-full flex-col items-center">
           <CheckCircleIcon className="size-10 text-success" />
